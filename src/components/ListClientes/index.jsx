4989db0a76b1af40123afaa1c3f5be0942de282a
Josias Silva Siqueira <josiassxz@hotmail.com>
2022-11-20 15:27:04 -0300

    feito filtro

--- src/components/ListClientes/index.jsx
+++ src/components/ListClientes/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import '../../App.css';
-import MaterialTable,{MTablePagination} from 'material-table'
+import MaterialTable, { MTablePagination } from 'material-table'
 import * as axios from 'axios';
-import {Grid, TablePagination} from "@material-ui/core";
+import { Grid, TablePagination } from "@material-ui/core";
 
 
 
@@ -13,37 +13,80 @@ function App() {
 
     const [data, setData] = useState([])
 
-    let totalDeveDinheiro = data.reduce((sum, data) => {
-        return sum + data.deveDinheiro;
-    }, 0);
+    const [dataFiltro, setDataFiltro] = useState([])
 
-    let totalDeveSucata = data.reduce((sum, data) => {
-        return sum + data.deveKg;
-    }, 0);
+    const [totalDeveDinheiro, setTotalDeveDinheiro] = useState(0)
+    const [totalDeveSucata, setTotalDeveSucata] = useState(0)
+    const [totalHaverSucata, setTotalHaverSucata] = useState(0)
 
-    let totalHaverSucata = data.reduce((sum, data) => {
-        return sum + data.haverSucata;
-    }, 0);
+    // let totalDeveDinheiro = 0
+    // let totalDeveSucata = 0
+    // let totalHaverSucata = 0
+
+
+    useEffect(() => {
+        calculaValores(data);
+    }, [data])
+
+    const calculaValores = (valores) => {
+        setTotalDeveDinheiro(valores.reduce((sum, data) => {
+            return sum + data.deveDinheiro;
+        }, 0));
+
+        setTotalDeveSucata(valores.reduce((sum, data) => {
+            return sum + data.deveKg;
+        }, 0));
+
+        setTotalHaverSucata(valores.reduce((sum, data) => {
+            return sum + data.haverSucata;
+        }, 0));
+    }
+
+
+
+
+    const search = (event) => {
+        if (event.length >= 3) {
+            let found = data.filter(obj => {
+                return obj.rota.toLowerCase() == event.toLowerCase();
+            });
+            setDataFiltro(found);
+            calculaValores(found)
+            console.log(found, 'event')
+        } else {
+            calculaValores(data);
+        }
+
+    }
+
+
+
+
+
+
+
 
 
 
     const columns = [
-        { title: "Cliente", field: "cliente" },
-        { title: "Deve Sucata", field: "deveKg" },
-        { title: "Deve Dinheiro", field: 'deveDinheiro',},
-        { title: "Haver Sucata", field: "haverSucata",},
+        { title: "Cliente", field: "cliente", filtering: false },
+        { title: "Deve Sucata", field: "deveKg", filtering: false, },
+        { title: "Deve Dinheiro", filtering: false, field: 'deveDinheiro', type: "currency", currencySetting: { currencyCode: "BRL", minimumFractionDigits: 0 } },
+        { title: "Haver Sucata", field: "haverSucata", filtering: false, },
         // { title: "Deve Boleto", field: "deveBoleto", },
-        { title: "Rota", field: "rota", },
-        { title: "Ultima Alteração", field:"ultimaAlteracao", type: "date",  disabled: true,
-            dateSetting: { locale: "pt-BR" },},
+        { title: "Rota", field: "rota", filtering: false },
+        {
+            title: "Ultima Alteração", filtering: false, field: "ultimaAlteracao", type: "date", editable: false,
+            dateSetting: { locale: "pt-BR" },
+        },
     ]
 
-    const getClientes = async () =>{
+    const getClientes = async () => {
         try {
             const response = await axios.get(url)
             setData(response.data);
 
-        } catch (error){
+        } catch (error) {
             console.log(error);
         }
     }
@@ -54,60 +97,75 @@ function App() {
     }, [])
 
 
+
     return (
         <div className="App">
             <MaterialTable
                 title="Lista de CLientes"
                 data={data}
+                onSearchChange={(event) => {
+                    search(event)
+                }}
                 columns={columns}
                 components={{
-                    Pagination:(props)=><div>
-                        <Grid container style={{padding:15,background: '#f5f5f5'}}>
+                    Pagination: (props) => <div>
+                        <Grid container style={{ padding: 15, background: '#f5f5f5' }}>
                             <Grid sm={6} item> Total deve sucata: {totalDeveSucata}  | Total deve dinheiro : {totalDeveDinheiro} | Total haver sucata : {totalHaverSucata}</Grid>
                         </Grid>
-                        <TablePagination{...props}/>
+                        <TablePagination{...props} />
                     </div>
                 }}
                 options={{
-                    actionsColumnIndex:-1, addRowPosition: "first",
-                    exportButton: true
+                    actionsColumnIndex: -1, addRowPosition: "first",
+                    exportButton: true,
+                    sorting: true,
+                    search: true,
+                    filtering: true,
+                    showEmptyDataSourceMessage: "Não há dados para carregar",
+                    showFirstLastPageButtons: false
+
+
+
                 }}
                 editable={{
-                     onRowAdd: (newData) => new Promise ((resolve, reject) => {
+                    onRowAdd: (newData) => new Promise((resolve, reject) => {
                         //BackEnd call
-                         fetch(url,{
+                        fetch(url, {
                             method: "POST",
-                            headers:{
+                            headers: {
                                 "Content-Type": "application/json"
                             },
                             body: JSON.stringify(newData)
-                        }).then(resp=>resp.json())
-                            .then(resp=>{getClientes()
+                        }).then(resp => resp.json())
+                            .then(resp => {
+                                getClientes()
                                 resolve();
                             })
                     }),
-                    onRowUpdate:(newData,oldData) => new Promise((resolve, reject) => {
+                    onRowUpdate: (newData, oldData) => new Promise((resolve, reject) => {
                         //BackEnd call
-                         fetch(url+'/'+oldData.id,{
+                        fetch(url + '/' + oldData.id, {
                             method: "PUT",
-                            headers:{
+                            headers: {
                                 "Content-Type": "application/json"
                             },
                             body: JSON.stringify(newData)
-                        }).then(()=>{getClientes()
-                                resolve();
-                            })
+                        }).then(() => {
+                            getClientes()
+                            resolve();
+                        })
                     }),
-                    onRowDelete:(oldData)=> new Promise((resolve, reject) => {
+                    onRowDelete: (oldData) => new Promise((resolve, reject) => {
                         //BackEnd call
-                           fetch(url+"/"+oldData.id,{
+                        fetch(url + "/" + oldData.id, {
                             method: "DELETE",
-                            headers:{
+                            headers: {
                                 "Content-Type": "application/json"
                             },
-                        }).then(()=>{getClientes()
-                                resolve();
-                            })
+                        }).then(() => {
+                            getClientes()
+                            resolve();
+                        })
                     })
                 }}
             />
